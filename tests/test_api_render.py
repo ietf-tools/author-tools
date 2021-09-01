1036dc5ca8ed8d6b57b37daee09ed3dda9878b76
Kesara Rathnayake <kesara@fq.nz>
2021-09-01 14:35:51 +1200

    Adds tests for /api/render/xml

--- tests/test_api_render.py
+++ tests/test_api_render.py
@@ -91,3 +91,15 @@ class TestApiRender(TestCase):
                 self.assertEqual(
                         json_data['error'],
                         'Input file format not supported')
+
+    def test_render_xml(self):
+        with self.app.test_client() as client:
+            with self.app.app_context():
+                for filename in TEST_DATA:
+                    result = client.post(
+                            '/api/render/xml',
+                            data={'file': (
+                                open(get_path(filename), 'rb'), filename)})
+
+                    self.assertEqual(result.status_code, 200)
+                    self.assertIsNotNone(result.data)
