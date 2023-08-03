541a3096eb8dd0a87c4e9c2d057036d890d44b4f
Kesara Rathnayake <kesara@fq.nz>
2023-08-03 23:55:47 +1200

    build: Use gunicorn

--- serve.py
+++
@@ -1,9 +0,0 @@
-from logging import getLogger, INFO
-from waitress import serve
-from at import create_app
-
-for log in ['waitress', 'at']:
-    logger = getLogger(log)
-    logger.setLevel(INFO)
-app = create_app()
-serve(app, listen='*:80')
