eb53f66dcbe1d404f2401ec7e0de0c25b37925a4
Kesara Rathnayake <kesara@fq.nz>
2026-07-07 08:08:00 +1200

    feat: Use gevent (#658)
    Update to gunicorn 26.0.0 and use gevent

--- docker/gunicorn.py
+++ docker/gunicorn.py
@@ -3,6 +3,7 @@ import os
 accesslog = "-"
 errorlog = "-"
 capture_output = True
+control_socket_disable = True
 workers = os.getenv("GUNICORN_WORKERS", 2)
-worker_class = "eventlet"
+worker_class = "gevent"
 bind = "0.0.0.0:8008"
